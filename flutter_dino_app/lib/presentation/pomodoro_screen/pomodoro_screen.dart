c3f59da960a32b847eaf0dd8f25b4d0f693f43c3
Swann HERRERA <swann@devloup.dev>
2022-10-19 11:48:07 +0200

    format display

--- flutter_dino_app/lib/presentation/pomodoro_screen/pomodoro_screen.dart
+++ flutter_dino_app/lib/presentation/pomodoro_screen/pomodoro_screen.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
+const Duration defaultPeriod = Duration(hours: 1, minutes: 25);
+
+extension GetAsString on Duration {
+  String inSecondWithMinutes() {
+    final Duration duration = this;
+    if (duration.inHours > 0) {
+      return '${duration.inHours}:${duration.inMinutes.remainder(60).toString().padLeft(2, '0')}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}';
+    } else {
+      return '${duration.inMinutes.remainder(60).toString().padLeft(2, '0')}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}';
+    }
+  }
+}
+
 abstract class Timer {
   Stream<Duration> start(Duration period);
   void stop();
@@ -30,8 +43,7 @@ final timerProvider = Provider<Timer>((ref) => DefaultTimer());
 
 final tickerProvider = StreamProvider<Duration>((ref) {
   final timer = ref.watch(timerProvider);
-  const startDuration = Duration(minutes: 25);
-  return timer.start(startDuration);
+  return timer.start(defaultPeriod);
 });
 
 class PomodoroScreen extends ConsumerWidget {
@@ -56,8 +68,8 @@ class PomodoroScreen extends ConsumerWidget {
         ),
       ),
       min: 0,
-      max: 60,
-      initialValue: 10,
+      max: 1000,
+      initialValue: 0,
       onChange: (double value) {
         // callback providing a value while its being changed (with a pan gesture)
       },
@@ -68,12 +80,15 @@ class PomodoroScreen extends ConsumerWidget {
         // ucallback providing an ending value (when a pan gesture ends)
       },
       innerWidget: (double value) {
-        final res = timer.when(
+        final Duration remainingTime = timer.when(
           data: (Duration value) => value,
-          error: (Object e, _) => e,
-          loading: () => 0, // Make default value
+          error: (Object e, _) {
+            print(e); // TODO add logger
+            return defaultPeriod;
+          },
+          loading: () => defaultPeriod,
         );
-        return Center(child: Text(res.toString()));
+        return Center(child: Text(remainingTime.inSecondWithMinutes()));
         // use your custom widget inside the slider (gets a slider value from the callback)
       },
     );
