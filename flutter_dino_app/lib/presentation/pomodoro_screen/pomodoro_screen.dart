2450cc169ad91d7711deef63e59edcdc48d097f9
Swann HERRERA <swann@devloup.dev>
2022-10-22 11:01:44 +0200

    add timer code from v1

--- flutter_dino_app/lib/presentation/pomodoro_screen/pomodoro_screen.dart
+++ flutter_dino_app/lib/presentation/pomodoro_screen/pomodoro_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dino_app/presentation/pomodoro_screen/widgets/growing_tree.dart';
+import 'package:flutter_dino_app/presentation/theme/theme.dart';
+import 'package:flutter_dino_app/presentation/widgets/circular_progress_indicator.dart';
+import 'package:flutter_dino_app/utils/duration.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../state/timer/timer.dart';
 
 const Duration defaultPeriod = Duration(minutes: 1);
 
@@ -7,9 +14,46 @@ double computeCurrentValue(Duration remainingTime) {
   return gap.toDouble();
 }
 
-class PomodoroScreen extends StatelessWidget {
+class PomodoroScreen extends ConsumerWidget {
   const PomodoroScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<Duration> timer = ref.watch(tickerProvider);
+    final Duration remainingTime = timer.when(
+      data: (Duration value) => value,
+      error: (Object e, _) {
+        print(e); // TODO add logger
+        return defaultPeriod;
+      },
+      loading: () => defaultPeriod,
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(appTitle),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircularProgressTimer(
+              remainingTime: remainingTime,
+              children: Center(
+                child: Text(
+                  remainingTime.inSecondsWithMinutes(),
+                  style: const TextStyle(
+                    fontSize: 40,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const GrowingTree()
+          ],
+        ),
+      ),
+    );
+  }
 }
