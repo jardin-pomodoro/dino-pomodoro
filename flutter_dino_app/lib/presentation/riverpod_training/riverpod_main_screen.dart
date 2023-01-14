0e531a9dde622d76981f28623e4e2922be40c157
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-01-14 16:57:28 +0100

    create date state

--- flutter_dino_app/lib/presentation/riverpod_training/riverpod_main_screen.dart
+++ flutter_dino_app/lib/presentation/riverpod_training/riverpod_main_screen.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dino_app/presentation/state/time/date.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class RiverPodMainScreen extends StatelessWidget {
-  const RiverPodMainScreen({Key? key}) : super(key: key);
+class RiverPodMainScreen extends ConsumerWidget {
+  const RiverPodMainScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(currentDate);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Riverpod'),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'The current date is:',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(width: 10),
+            Text(
+              '${date.day}/${date.month}/${date.year} ${date.hour}:${date.minute}:${date.second}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
     );
   }
