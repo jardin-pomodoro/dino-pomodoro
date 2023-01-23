e93dc4b3241a7efe12ba98a0307ee4a31d963cce
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-01-23 21:37:39 +0100

    create seed type model

---
+++ flutter_dino_app/lib/domain/models/seed_type.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class SeedType {
+  final String id;
+  final String name;
+  final String image;
+  final int timeToGrow;
+  final int price;
+  final int reward;
+  final int leafMaxUpgrades;
+  final int trunkMaxUpgrades;
+  final DateTime created;
+  final DateTime updated;
+
+  const SeedType({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.timeToGrow,
+    required this.price,
+    required this.reward,
+    required this.leafMaxUpgrades,
+    required this.trunkMaxUpgrades,
+    required this.created,
+    required this.updated,
+  });
+}
