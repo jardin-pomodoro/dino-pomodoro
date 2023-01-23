a2a3c24c8c5ee7bfb3fa92713962c5e972ae06b3
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-01-23 20:26:47 +0100

    set on theme

--- flutter_dino_app/lib/main.dart
+++ flutter_dino_app/lib/main.dart
@@ -1,23 +1,35 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dino_app/presentation/router.dart';
 import 'package:flutter_dino_app/presentation/theme/theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:json_theme/json_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: MyApp()));
+
+  final themeStr = await rootBundle.loadString('assets/theme.json');
+  final themeJson = jsonDecode(themeStr);
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
+
+  runApp(ProviderScope(child: MyApp(theme: theme)));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ThemeData theme;
+
+  const MyApp({
+    Key? key,
+    required this.theme,
+  }): super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: appTitle,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+      theme: theme,
       routerConfig: router,
     );
   }
