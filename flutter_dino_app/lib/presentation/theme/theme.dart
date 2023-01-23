3d7cd450a82e3291c8700fb6a595833c223830bf
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-01-23 21:36:28 +0100

    add custom theme color

--- flutter_dino_app/lib/presentation/theme/theme.dart
+++ flutter_dino_app/lib/presentation/theme/theme.dart
@@ -1,1 +1,42 @@
+
+import 'package:flutter/material.dart';
+
 const appTitle = 'Dino pomodoro';
+
+class PomodoroTheme {
+  static const Color white = Color(0xFFFEFEFE);
+  static const Color primary = Color(0xff12b8a6);
+  static const Color secondary = Color(0xff087f5b);
+  static const Color accent = Color(0xff12b886);
+  static const Color yellow = Color(0xfffab005);
+
+  static const TextStyle title3 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: white,
+  );
+
+  static const TextStyle title4 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: white,
+  );
+
+  static const TextStyle text = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: white,
+  );
+
+  static const TextStyle textBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    color: white,
+  );
+
+  static const TextStyle textLarge = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+    color: white,
+  );
+}
