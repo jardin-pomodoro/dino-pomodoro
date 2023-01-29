fd4a742f2a141f51adef19fdddd251bc2ae60454
Rémy Machavoine <remygt@hotmail.fr>
2023-01-29 18:14:02 +0100

    add visual working graph

--- flutter_dino_app/lib/presentation/screen/forest_screen/widget/calendar_chart.dart
+++ flutter_dino_app/lib/presentation/screen/forest_screen/widget/calendar_chart.dart
@@ -1,125 +1,135 @@
 import 'package:flutter/cupertino.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dino_app/presentation/theme/theme.dart';
 
 class CalendarChart extends StatelessWidget {
   const CalendarChart({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return BarChart(
-      BarChartData(
-        alignment: BarChartAlignment.spaceAround,
-        maxY: 10,
-        barTouchData: BarTouchData(
-          enabled: false,
-        ),
-        titlesData: FlTitlesData(
-          show: true,
-          bottomTitles: AxisTitles(
-            sideTitles: SideTitles(
-              showTitles: true,
-              reservedSize: 36,
-              getTitlesWidget: (value, meta) {
-                final index = value.toInt();
-                return SideTitleWidget(
-                  axisSide: meta.axisSide,
-                  child: Text(
-                    'SMTWTFS'[index],
-                    style: const TextStyle(
-                      color: Color(0xff7589a2),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                    ),
-                  ),
-                );
-              },
+    return AspectRatio(
+      aspectRatio: 2,
+      child: BarChart(
+        BarChartData(
+          barGroups: [
+            BarChartGroupData(
+              x: 0,
+              barRods: [
+                BarChartRodData(
+                  toY: 10.0,
+                  color: PomodoroTheme.white,
+                  width: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ],
             ),
+            BarChartGroupData(
+              x: 1,
+              barRods: [
+                BarChartRodData(
+                  toY: 15.0,
+                  color: PomodoroTheme.white,
+                  width: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ],
+            ),
+            BarChartGroupData(
+              x: 2,
+              barRods: [
+                BarChartRodData(
+                  toY: 10.0,
+                  color: PomodoroTheme.white,
+                  width: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ],
+            ),
+            BarChartGroupData(
+              x: 3,
+              barRods: [
+                BarChartRodData(
+                  toY: 10.0,
+                  color: PomodoroTheme.white,
+                  width: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ],
+            ),
+            BarChartGroupData(
+              x: 4,
+              barRods: [
+                BarChartRodData(
+                  toY: 10,
+                  color: PomodoroTheme.white,
+                  width: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ],
+            ),
+            BarChartGroupData(
+              x: 5,
+              barRods: [
+                BarChartRodData(
+                  toY: 10,
+                  color: PomodoroTheme.white,
+                  width: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ],
+            ),
+            BarChartGroupData(
+              x: 6,
+              barRods: [
+                BarChartRodData(
+                  toY: 10,
+                  color: PomodoroTheme.white,
+                  width: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ],
+            ),
+          ],
+          borderData: FlBorderData(
+              border: const Border(bottom: BorderSide(), left: BorderSide())),
+          gridData: FlGridData(show: false),
+          titlesData: FlTitlesData(
+            bottomTitles: AxisTitles(sideTitles: _bottomTitles),
+            leftTitles: AxisTitles(),
+            rightTitles: AxisTitles(),
+            topTitles: AxisTitles(),
           ),
-          leftTitles: AxisTitles(),
         ),
-        borderData: FlBorderData(
-          show: false,
-        ),
-        barGroups: [
-          BarChartGroupData(
-            x: 0,
-            barRods: [
-              BarChartRodData(
-                toY: 5,
-                color: Color(0xff4af699),
-                width: 18,
-              ),
-            ],
-            showingTooltipIndicators: [0],
-          ),
-          BarChartGroupData(
-            x: 1,
-            barRods: [
-              BarChartRodData(
-                toY: 13,
-                color: Color(0xff4af699),
-                width: 18,
-              ),
-            ],
-            showingTooltipIndicators: [0],
-          ),
-          BarChartGroupData(
-            x: 2,
-            barRods: [
-              BarChartRodData(
-                toY: 10,
-                color: Color(0xff4af699),
-                width: 18,
-              ),
-            ],
-            showingTooltipIndicators: [0],
-          ),
-          BarChartGroupData(
-            x: 3,
-            barRods: [
-              BarChartRodData(
-                toY: 4,
-                color: Color(0xff4af699),
-                width: 18,
-              ),
-            ],
-            showingTooltipIndicators: [0],
-          ),
-          BarChartGroupData(
-            x: 4,
-            barRods: [
-              BarChartRodData(
-                color: Color(0xff4af699),
-                width: 18,
-                toY: 8,
-              ),
-            ],
-            showingTooltipIndicators: [0],
-          ),
-          BarChartGroupData(
-            x: 5,
-            barRods: [
-              BarChartRodData(
-                color: Color(0xff4af699),
-                width: 12,
-                toY: 9,
-              ),
-            ],
-            showingTooltipIndicators: [0],
-          ),
-          BarChartGroupData(
-            x: 6,
-            barRods: [
-              BarChartRodData(
-                color: Color(0xff4af699),
-                width: 12,
-                toY: 9,
-              ),
-            ],
-            showingTooltipIndicators: [0],
-          ),
-        ],
       ),
     );
   }
+
+  SideTitles get _bottomTitles => SideTitles(
+    showTitles: true,
+    getTitlesWidget: (value, meta) {
+      String text = '';
+      switch (value.toInt()) {
+        case 0:
+          text = 'Jan';
+          break;
+        case 2:
+          text = 'Mar';
+          break;
+        case 4:
+          text = 'May';
+          break;
+        case 6:
+          text = 'Jul';
+          break;
+        case 8:
+          text = 'Sep';
+          break;
+        case 10:
+          text = 'Nov';
+          break;
+      }
+
+      return Text(text);
+    },
+  );
 }
