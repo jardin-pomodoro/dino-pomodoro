913a53e47609a15d6c393bab20b1259174db4e86
Rémy Machavoine <remygt@hotmail.fr>
2023-01-29 20:04:23 +0100

    add evolutive graph

--- flutter_dino_app/lib/presentation/screen/forest_screen/forest_screen_widget.dart
+++ flutter_dino_app/lib/presentation/screen/forest_screen/forest_screen_widget.dart
@@ -20,33 +20,66 @@ class ForestScreenWidget extends StatefulWidget {
 
 class _ForestScreenWidgetState extends State<ForestScreenWidget> {
   String slidingChoice = "Jour";
+  String granularity = "day";
+  List<int> dataByGranularity = [
+    10, 14, 8, 2, 19, 39, 09, 19, 29, 63, 05, 72, 27, 28, 28, 27, 29, 63, 05, 72, 27, 28, 28, 27
+  ];
+  late List<int> dataPast = [
+    10, 14, 8, 2, 19, 39, 09, 19, 29, 63, 05, 72, 27, 28, 28, 27, 29, 63, 05, 72, 27, 28, 28, 27
+  ];
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return SizedBox(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-            child: SliderChoice(
-              items: const [
-                "Jour",
-                "Semaine",
-                "Mois",
-                "Année",
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                  child: SliderChoice(
+                    items: const [
+                      "Jour",
+                      "Semaine",
+                      "Mois",
+                      "Année",
+                    ],
+                    changeSlidingChoice: _changeSlidingChoice,
+                  ),
+                ),
+                //const SwipeArrow(),
+                ListHorizontalSlide(
+                  treesStatsUi: [
+                    TreeStatsUi(
+                        image:
+                            'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/klfch9yk075cmpq/canvas_removebg_preview_zRg2OoMJsv.png',
+                        number: 10),
+                    TreeStatsUi(
+                        image:
+                            'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/q93aghxz9h1pl7u/canvas3_removebg_preview_AsHORCGEJN.png',
+                        number: 5),
+                    TreeStatsUi(
+                        image:
+                            'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/klfch9yk075cmpq/canvas_removebg_preview_zRg2OoMJsv.png',
+                        number: 8),
+                    TreeStatsUi(
+                        image:
+                            'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/q93aghxz9h1pl7u/canvas3_removebg_preview_AsHORCGEJN.png',
+                        number: 9),
+                  ],
+                ),
               ],
-              changeSlidingChoice: _changeSlidingChoice,
             ),
           ),
-          SwipeArrow(),
-          ListHorizontalSlide(
-            treesStatsUi: [
-              TreeStatsUi(image: 'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/klfch9yk075cmpq/canvas_removebg_preview_zRg2OoMJsv.png', number: 10),
-              TreeStatsUi(image: 'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/q93aghxz9h1pl7u/canvas3_removebg_preview_AsHORCGEJN.png', number: 5),
-              TreeStatsUi(image: 'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/klfch9yk075cmpq/canvas_removebg_preview_zRg2OoMJsv.png', number: 8),
-              TreeStatsUi(image: 'https://pocketbase.nospy.fr/api/files/lajospkke93eknf/q93aghxz9h1pl7u/canvas3_removebg_preview_AsHORCGEJN.png', number: 9),
-            ],
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: CalendarChart(
+              granularity: granularity,
+              dataByGranularity: dataPast,
+            ),
           ),
-          CalendarChart(),
         ],
       ),
     );
@@ -55,6 +88,20 @@ class _ForestScreenWidgetState extends State<ForestScreenWidget> {
   void _changeSlidingChoice(String choice) {
     setState(() {
       slidingChoice = choice;
+      if(slidingChoice == "Jour") {
+        granularity = "day";
+        dataPast = dataByGranularity.sublist(0, 24);
+      } else if (slidingChoice == "Semaine") {
+        granularity = "week";
+        dataPast = dataByGranularity.sublist(0, 7);
+
+      } else if (slidingChoice == "Mois") {
+        granularity = "month";
+        dataPast = dataByGranularity.sublist(0, 12);
+      } else if (slidingChoice == "Année") {
+        granularity = "year";
+        dataPast = dataByGranularity.sublist(0, 12);
+      }
     });
   }
 }
--- flutter_dino_app/lib/presentation/screen/forest_screen/widget/calendar_chart.dart
+++ flutter_dino_app/lib/presentation/screen/forest_screen/widget/calendar_chart.dart
@@ -3,98 +3,34 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dino_app/presentation/theme/theme.dart';
 
-class CalendarChart extends StatelessWidget {
-  const CalendarChart({Key? key}) : super(key: key);
+class CalendarChart extends StatefulWidget {
+  final String granularity;
+  final List<int> dataByGranularity;
+  const CalendarChart({
+    Key? key,
+    required this.granularity,
+    required this.dataByGranularity,
+  }) : super(key: key);
+
+  @override
+  State<CalendarChart> createState() => _CalendarChartState();
+}
+
+class _CalendarChartState extends State<CalendarChart> {
+
+
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2,
       child: BarChart(
         BarChartData(
-          barGroups: [
-            BarChartGroupData(
-              x: 0,
-              barRods: [
-                BarChartRodData(
-                  toY: 10.0,
-                  color: PomodoroTheme.white,
-                  width: 10,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ],
-            ),
-            BarChartGroupData(
-              x: 1,
-              barRods: [
-                BarChartRodData(
-                  toY: 15.0,
-                  color: PomodoroTheme.white,
-                  width: 10,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ],
-            ),
-            BarChartGroupData(
-              x: 2,
-              barRods: [
-                BarChartRodData(
-                  toY: 10.0,
-                  color: PomodoroTheme.white,
-                  width: 10,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ],
-            ),
-            BarChartGroupData(
-              x: 3,
-              barRods: [
-                BarChartRodData(
-                  toY: 10.0,
-                  color: PomodoroTheme.white,
-                  width: 10,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ],
-            ),
-            BarChartGroupData(
-              x: 4,
-              barRods: [
-                BarChartRodData(
-                  toY: 10,
-                  color: PomodoroTheme.white,
-                  width: 10,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ],
-            ),
-            BarChartGroupData(
-              x: 5,
-              barRods: [
-                BarChartRodData(
-                  toY: 10,
-                  color: PomodoroTheme.white,
-                  width: 10,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ],
-            ),
-            BarChartGroupData(
-              x: 6,
-              barRods: [
-                BarChartRodData(
-                  toY: 10,
-                  color: PomodoroTheme.white,
-                  width: 10,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ],
-            ),
-          ],
+          barGroups: _chartGroups(widget.granularity, widget.dataByGranularity),
           borderData: FlBorderData(
               border: const Border(bottom: BorderSide(), left: BorderSide())),
           gridData: FlGridData(show: false),
           titlesData: FlTitlesData(
-            bottomTitles: AxisTitles(sideTitles: _bottomTitles),
+            bottomTitles: AxisTitles(sideTitles: _sideTitles(widget.granularity)),
             leftTitles: AxisTitles(),
             rightTitles: AxisTitles(),
             topTitles: AxisTitles(),
@@ -104,7 +40,7 @@ class CalendarChart extends StatelessWidget {
     );
   }
 
-  SideTitles get _bottomTitles => SideTitles(
+  SideTitles get _monthTitles => SideTitles(
     showTitles: true,
     getTitlesWidget: (value, meta) {
       String text = '';
@@ -112,24 +48,159 @@ class CalendarChart extends StatelessWidget {
         case 0:
           text = 'Jan';
           break;
+        case 1:
+          text = 'Fev';
+          break;
         case 2:
           text = 'Mar';
           break;
+        case 3:
+          text = 'Avr';
+          break;
         case 4:
-          text = 'May';
+          text = 'Mai';
+          break;
+        case 5:
+          text = 'Jui';
           break;
         case 6:
-          text = 'Jul';
+          text = 'Jui';
+          break;
+        case 7:
+          text = 'Aoû';
           break;
         case 8:
           text = 'Sep';
           break;
+        case 9:
+          text = 'Oct';
+          break;
         case 10:
           text = 'Nov';
+          break;
+        case 11:
+          text = 'Déc';
           break;
       }
 
       return Text(text);
     },
   );
+
+  SideTitles get _dayTitles => SideTitles(
+    showTitles: true,
+    getTitlesWidget: (value, meta) {
+      String text = '';
+      switch (value.toInt()) {
+        case 0:
+          text = '0h';
+          break;
+        case 2:
+          text = '2';
+          break;
+        case 4:
+          text = '4';
+          break;
+        case 6:
+          text = '6';
+          break;
+        case 8:
+          text = '8';
+          break;
+        case 10:
+          text = '10';
+          break;
+        case 12:
+          text = '12';
+          break;
+        case 14:
+          text = '14';
+          break;
+        case 16:
+          text = '16';
+          break;
+        case 18:
+          text = '18';
+          break;
+        case 20:
+          text = '20';
+          break;
+        case 22:
+          text = '22';
+          break;
+      }
+
+      return Text(text);
+    },
+  );
+
+  SideTitles get _weekTitles => SideTitles(
+    showTitles: true,
+    getTitlesWidget: (value, meta) {
+      String text = '';
+      switch (value.toInt()) {
+        case 0:
+          text = 'Lun';
+          break;
+        case 1:
+          text = 'Mar';
+          break;
+        case 2:
+          text = 'Mer';
+          break;
+        case 3:
+          text = 'Jeu';
+          break;
+        case 4:
+          text = 'Ven';
+          break;
+        case 5:
+          text = 'Sam';
+          break;
+        case 6:
+          text = 'Dim';
+          break;
+      }
+
+      return Text(text);
+    },
+  );
+
+  List<BarChartGroupData> _chartGroups(String granularity, List<int> dataByGranularity) {
+    final List fixedList = Iterable<int>.generate(dataByGranularity.length).toList();
+    final double barChartWidth;
+    if (granularity == "day" || granularity == "month") {
+      barChartWidth = 10;
+    } else if(granularity == "week" || granularity == "year") {
+      barChartWidth = 20;
+    } else {
+      barChartWidth = 10;
+    }
+    return fixedList.map((index) =>
+        BarChartGroupData(
+          x: index,
+          barRods: [
+            BarChartRodData(
+              toY: dataByGranularity[index].toDouble(),
+              color: PomodoroTheme.white,
+              width: barChartWidth,
+              borderRadius: BorderRadius.only(topRight: Radius.circular(5), topLeft: Radius.circular(5)),
+            ),
+          ],
+        ),
+    ).toList();
+  }
+
+  SideTitles _sideTitles(String granularity) {
+    print("je passe ici");
+    print(granularity);
+    if(granularity == 'day') {
+      return _dayTitles;
+    } else if (granularity == 'week'){
+      return _weekTitles;
+    } else if (granularity == 'month'){
+      return _monthTitles;
+    }
+    return _monthTitles;
+  }
 }
