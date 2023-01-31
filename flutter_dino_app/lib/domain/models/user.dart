cedb67e3502f8cc5e138c1af5188bc4f361c241d
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-01-31 19:41:20 +0100

    Create user model

---
+++ flutter_dino_app/lib/domain/models/user.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class User {
+  final String collectionId;
+  final String collectionName;
+  final String id;
+  final String username;
+  final String email;
+  final String avatar;
+  final DateTime created;
+  final DateTime updated;
+
+  const User({
+    required this.collectionId,
+    required this.collectionName,
+    required this.created,
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.avatar,
+    required this.updated,
+  });
+
+  User copyWith({
+    String? username,
+    String? avatar,
+    String? email,
+  }) {
+    return User(
+      collectionId: collectionId,
+      collectionName: collectionName,
+      created: created,
+      id: id,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      avatar: avatar ?? this.avatar,
+      updated: updated,
+    );
+  }
+}
