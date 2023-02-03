865fe143cb06e53e26edd94ef072d72aab6c7c0f
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-02-03 15:46:24 +0100

    create grow failed dialog widget

---
+++ flutter_dino_app/lib/presentation/screen/growing_screen/widgets/grow_failed_dialog_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../../../widgets/price_widget.dart';
+
+import '../../../../domain/models/tree.dart';
+import '../../../theme/theme.dart';
+
+class GrowFailedDialogWidget extends StatelessWidget {
+  const GrowFailedDialogWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: PomodoroTheme.accent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+        side: const BorderSide(
+          color: PomodoroTheme.white,
+          width: 2,
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset("assets/images/dead_tree.png"),
+            const Text(
+              "Oh non !",
+              style: PomodoroTheme.title3,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Votre arbre est mort...",
+              style: PomodoroTheme.title4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
