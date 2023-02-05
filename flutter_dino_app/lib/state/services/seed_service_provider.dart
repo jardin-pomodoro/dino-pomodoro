aaa698b4c3bb33a4fa651d63889740551c372731
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-02-05 09:26:31 +0100

    add services

---
+++ flutter_dino_app/lib/state/services/seed_service_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_dino_app/state/pomodoro_states/auth_state_notifier.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../core/success.dart';
+import '../../data/datasource/api/repositories/api_seed_repository.dart';
+import '../../data/datasource/local/database/database_source.dart';
+import '../../data/datasource/local/repositories/local_seed_repository.dart';
+import '../../domain/models/seed.dart';
+import '../../domain/services/seed_service.dart';
+import '../api_consumer/api_consumer.dart';
+import '../db/local_db_provider.dart';
+
+final seedServiceProvider = Provider<SeedService>((ref) {
+  final consumer = ref.read(apiProvider);
+  final DatabaseSource db = ref.read(localDbProvider);
+  final seedRepository = ApiSeedRepository(consumer);
+  final localSeedRepository = LocalSeedRepository(db);
+
+  return SeedService(seedRepository, localSeedRepository);
+});
+
+final fetchSeedsProvider = FutureProvider<Success<List<Seed>>>((ref) async {
+  final userAuth = ref.watch(authStateNotifierProvider);
+  final seedService = ref.read(seedServiceProvider);
+  return await seedService.getSeeds(userAuth.user.id);
+});
