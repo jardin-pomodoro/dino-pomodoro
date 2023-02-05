893b4468e60c6dc0e6889e279c8109eb7098f701
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-02-05 13:18:00 +0100

    grow repo + update tree repo

--- flutter_dino_app/lib/domain/repositories/tree_repository.dart
+++ flutter_dino_app/lib/domain/repositories/tree_repository.dart
@@ -1,7 +1,10 @@
 import '../../core/success.dart';
+import '../models/growing.dart';
 import '../models/tree.dart';
 
 abstract class TreeRepository {
   Future<Success<List<Tree>>> retrieveTreeRepository(
       String userId, DateTime startDate, DateTime endDate);
+
+  Future<Success<Tree>> addNewTree(String userId, Growing growing);
 }
